81a9a8a96b6ad83290cd19f593e989f64d6d85a4
Da-mell Faines <damellfaines8@gmail.com>
2021-01-13 18:53:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
 # Write your methods here
+
+  def loop_message_five_times(string)
+    5.times do
+      puts string
+    end
+
+  end
+
+
+def loop_message_n_times(string,n)
+  n.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  for i in array do
+    puts array[]
+  end
+end
